6ad491c3d966d9af0239cd596e681ce4591b686f
Alexey <aspodolsky@ya.ru>
2022-04-18 11:16:00 +0300

    initial

---
+++ index.ts
@@ -0,0 +1,64 @@
+import { PrismaClient } from '@prisma/client';
+import { DataSource } from 'apollo-datasource';
+import { KeyValueCache } from 'apollo-server-caching';
+import * as crypto from 'crypto';
+import { MiddlewareParams } from "./types";
+
+
+type CacheQuery = {
+  ttl: number,
+}
+
+export class PrismaDataSource extends DataSource {
+  public prisma: PrismaClient;
+  context: Object | undefined;
+  cache: KeyValueCache | undefined;
+  ttlDefault: number;
+  isInitialized: boolean = false;
+
+  constructor(prismaClient: PrismaClient, defaultCacheTtl: number = 10) {
+    super();
+    this.ttlDefault = defaultCacheTtl;
+    this.prisma = prismaClient;
+  }
+
+  async initialize(config: { context: Object | undefined; cache: KeyValueCache | undefined; }) {
+    this.context = config.context;
+    this.cache = config.cache;
+
+    if (!this.isInitialized && this.cache) {
+      let result;
+      this.prisma.$use(async (params: MiddlewareParams, next: <T>(params: MiddlewareParams) => Promise<T>) => {
+        const { cache, ...query }: { cache: CacheQuery | undefined, query: unknown } = params.args;
+        delete params.args.cache;
+
+        if ( cache !== undefined) {
+          const queryHash = this.createHash({
+            [params.model]: {
+              [params.action]: query,
+            }
+          });
+          const cachedData = await this.getCache(queryHash);
+
+          if (cachedData) {
+            return JSON.parse(cachedData);
+          }
+          result = await next(params);
+          this.cache?.set(queryHash, JSON.stringify(result), { ttl: cache.ttl ?? this.ttlDefault });
+        } else {
+          result = await next(params);
+        }
+
+        return result;
+      });
+    }
+  }
+
+  createHash(query: any): string {
+    return crypto.createHash('sha1').update(JSON.stringify(query)).digest('base64');
+  }
+
+  async getCache(key: string) {
+    return this.cache?.get(key);
+  }
+}
---
+++ types.ts
@@ -0,0 +1,27 @@
+export type PrismaQueryAction =
+    | 'findUnique'
+    | 'findFirst'
+    | 'findMany'
+    | 'findRaw'
+    | 'aggregate'
+    | 'aggregateRaw'
+    | 'count'
+    | 'groupBy';
+
+export type PrismaMutationAction =
+    | 'create'
+    | 'createMany'
+    | 'update'
+    | 'updateMany'
+    | 'upsert'
+    | 'delete'
+    | 'deleteMany'
+    | 'executeRawUnsafe';
+
+export type MiddlewareParams = {
+  model: string;
+  action: PrismaQueryAction | PrismaMutationAction;
+  args: any;
+  dataPath: string[];
+  runInTransaction: boolean;
+};
